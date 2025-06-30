de042b50b016d88cd74337dd3ec67b54fccc8c08
Mayank Negi <mayanknegi15011@gmail.com>
2025-06-30 17:04:45 +0530

    other section

--- src/components/OfferBanner.jsx
+++ src/components/OfferBanner.jsx
@@ -1,7 +1,8 @@
 import { useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { useGSAP } from '@gsap/react'
+import { useGSAP } from '@gsap/react';
+import buy from '../assets/images/anc.svg';
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -23,12 +24,18 @@ const OfferBanner = () => {
 
   return (
     <div
-      className=" bg-gradient-to-b to-[#031C19] from-gray-800 text-white py-16 px-6 text-center -z-10"
+      className="w-full mx-auto bg-white text-black py-12 px-6 flex flex-col md:flex-row justify-around items-center gap-10 md:gap-6 text-center md:text-left"
       ref={container}
     >
-      <h2 className="text-4xl font-bold mb-4 font-['raleway']">Launch Offer: Flat 50% Off</h2>
-      <p className="mb-6 text-lg">Grab your Soniquo wireless headphones today. Limited time only!</p>
-      <a href="#buy" className="bg-white text-blue-700 font-semibold px-8 py-3 rounded-full hover:bg-gray-200 transition font-['inter']">Buy Now for ₹3,999</a>
+      <div className="left w-full md:w-1/2 order-2 md:order-1">
+        <h2 className="text-4xl md:text-5xl font-bold mb-4 font-['raleway']">Launch Offer: Flat 50% Off</h2>
+        <p className="mb-8 text-lg">Grab your Soniquo wireless headphones today. Limited time only!</p>
+        <a href="#buy" className="bg-black text-white font-semibold px-8 py-3 rounded-full hover:bg-gray-200 hover:text-black transition-colors duration-300 font-['inter']">Buy Now for ₹3,999</a>
+      </div>
+
+      <div className="w-full md:w-1/2 flex justify-center order-1 md:order-2">
+        <img src={buy} alt="Wireless Headphones Offer" className="w-[15em] max-w-xs md:max-w-sm" />
+      </div>
     </div>
   )
 }
